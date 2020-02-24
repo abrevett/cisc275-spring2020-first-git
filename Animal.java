ea2d6ba4730e04831489a7a535815af8860fc55e
abrevett <abrevett@aol.com>
2020-02-24 13:54:00 -0500

    Added Animal.java: created an abstract class for use in Dog class

---
+++ Animal.java
@@ -0,0 +1,13 @@
+abstract class Animal{
+	String name;
+	int legs;
+
+	public Animal(String name, int legs){
+		this.name = name;
+		this.legs = legs;
+	}
+
+	public int getLegs(){
+		return this.legs;
+	}
+}
